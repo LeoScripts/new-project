00ebce124fb847ab151cbd5d82228fcdc0719c39
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-09 11:03:43 -0300

    base

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.vue,.js,.ts,.jsx,.tsx,.sass,.css', 
+    './src/**/*.vue,.js,.ts,.jsx,.tsx,.sass,.css',
+    './components/**/*.vue,.js,.ts,.jsx,.tsx,.sass,.css',
+  ],
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
